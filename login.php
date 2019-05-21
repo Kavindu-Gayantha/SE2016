ce4c0c6cdb1c009e8185f2d51235b47f73abaf25
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-21 09:19:10 +0530

    added the header of the login page

---
+++ login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+  <title>SE-2016</title>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' href="login.css">
+</head>
+<body>
+  <div class='page-header'>
+    <h1> WELCOME to SE 2016 database web</h1>
+  </div>
+  <button type = "button" class = "btn btn-success" type="padding-left:5px; margin-top:0px;">Success Button</button>
+
+
+</body>
+</html>
