fb34ede5bf1e9249daffca794364aa41f769b2ab
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-22 13:04:09 +0530

    added connection to db

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+  $server ="localhost";
+  $db= "SEweb";
+  $user="root";
+  $password="";
+
+  $conn = mysqli_connection("$server","$db","$user",""$password") or die("connection failed");
+
+ ?>
