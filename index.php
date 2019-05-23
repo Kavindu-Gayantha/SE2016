436557e8717fe75a42a32aedb1d709f3e4c119ea
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-23 23:20:19 +0530

    finished designed new login page named index.php

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 <html lang="en">
 <head><title>log in</title>
   <meta charset="utf-8">
+  <link rel="stylesheet" href="main.css">
 </head>
 <body>
+  <div class="header">
+    <h1 class="header_statement">SOFTWARE ENGINEERING <br>university of Kelaniya</h1>
+  </div>
   <div class="login">
     <form action="index.php" method="post">
       <fieldset>
         <legend><h1>Log In</h1></legend>
+
+       <!--<p class="error">Invalid Username / Password</p>-->
 
         <p>
           <label for="">Username:</label> <!--user name-->
