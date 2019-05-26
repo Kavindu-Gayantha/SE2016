4af049787ef16d033dff0be36cc0c33416095023
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-26 19:38:43 +0530

    add some changes

---
+++ includes/displaygreetings.php
@@ -0,0 +1,20 @@
+<?php
+  function display_greetings()
+  {
+    $hour = date('12h'); // give the hour of current time
+
+    if($hour >=0 and $hour <=11)
+    {
+      echo "Good Morning ! ";
+    }
+    elseif($hour >=12 and $hour <=17)
+    {
+      echo "Good Afternoon ! ";
+    }
+    else
+    {
+      echo "Good Evening ! ";
+    }
+  }
+
+ ?>
--- users.php
+++ users.php
@@ -1,5 +1,6 @@
 <?php session_start(); ?>
 <?php require_once('connection.php'); ?>
+<?php require_once('includes/displaygreetings.php'); ?>
 <?php
   // cheking if a user is logged in
   if(!isset($_SESSION['user_id']))
@@ -17,7 +18,7 @@
 <body>
   <header>
     <div class="appname">SE management system</div>
-    <div class="loggedin" style="color:white; font-size:22px;">Welcome <?php echo $_SESSION['first_name']; ?> ! <a href="logout.php" style="color:gold; font-weight:bold; font-size:20px;">logout</a></div>
+    <div class="loggedin" style="color:white; font-size:22px;"><?php display_greetings();?> <?php echo $_SESSION['first_name']; ?> ! <a href="logout.php" style="color:gold; font-weight:bold; font-size:20px;">logout</a></div>
   </header>
 
 
