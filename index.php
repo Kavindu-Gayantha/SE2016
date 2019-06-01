8ba6c2cf76b452e0004caccb89ff6f7d7e76e904
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-06-01 14:49:29 +0530

    added boostrap

--- index.php
+++ index.php
@@ -74,6 +74,8 @@ if(empty($errors))
 <head><title>log in</title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
   
 </head>
 <body>
