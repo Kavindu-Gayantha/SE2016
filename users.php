796c44f50752c4b255112590c59addbae40480f1
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-06-13 07:16:19 +0530

    added title with session tag of php

--- users.php
+++ users.php
@@ -11,6 +11,8 @@
 <!doctype html>
 <html lang="en">
 <head>
+
+<title>hi!! i'm <?php  echo $_SESSION['first_name']; ?></title>
    <!-- Required meta tags -->
    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
