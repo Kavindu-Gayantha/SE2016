642eb417feab67529943c189c85a4d507796d5bd
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-08-11 21:12:54 +0530

    added the infomation table to user.php page

--- users.php
+++ users.php
@@ -66,7 +66,14 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+<div class="details">
+  <table id="infotable" border="1px" width="60%">
+    <tr><td rowspan="4" width="40%"></td><td>Name : </td></tr>
+    <tr><td>Student number : </td></tr>
+    <tr><td>contact :</td></tr>
+    <tr><td>social :</td></tr>
+  </table>
+</div>
 
 </body>
 </html>
